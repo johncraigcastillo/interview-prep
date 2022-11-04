32688c67293e9439b40d923d339a4d7c2c4389d3
johncastillo <johncraigcastillo@gmail.com>
2022-11-04 00:28:45 -0500

    solved 26-remove-duplicates-sorted-array.py

---
+++ 26-remove-duplicates-sorted-array.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+"""
+Function must return the resulting length of the list after "removing"
+the duplicates
+
+input:  nums = [1,1,2]
+output: 2 nums = [1,2,_]   # _ means the value is irrelevant
+"""
+
+
+class Solution:
+    """
+    nums is sorted in increasing order
+    """
+    def removeDuplicates(self, nums: list[int]) -> int:
+        left = 1
+        for right in range(1, len(nums)):
+            if nums[right] != nums[right - 1]:
+                nums[left] = nums[right]
+                left += 1
+        return left
+
+
+nums = [0, 0, 1, 1, 2]
+# nums = [0]
+solution = Solution()
+k = solution.removeDuplicates(nums)
+print(f"nums: {nums}")
+print(f"k: {k}")
