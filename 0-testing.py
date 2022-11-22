f8a99bb1524fb232d8ef2adaef52e2f14243091a
johncastillo <johncraigcastillo@gmail.com>
2022-11-22 16:51:02 -0600

    Solved some of my past problems

--- 0-testing.py
+++ 0-testing.py
@@ -1,36 +1,36 @@
 """
-create empty set
+Use XOR ^ operator
+2 ^ 2 = 0
+2 ^ 2 ^ 1 = 1
+
+nums [2, 2, 1]
+result = 0
 for num in nums:
-    if num in set:
-        return True
-    add num to set
+    result ^= num
+return result
 """
 
 
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
-        numSet = set()
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
         for num in nums:
-            if num in numSet:
-                return True
-            numSet.add(num)
-        return False
+            result ^= num
+        return result
 
 
 def main():
-    nums = [1, 2, 3]
+    nums = [4, 1, 2, 1, 2]
     solution = Solution()
-    print(solution.containsDuplicate(nums))
+    print(solution.singleNumber(nums))
 
 
 if __name__ == "__main__":
     main()
 
-
 """
-time complexity: O(n)
-    inserting into set and checking if in a hash set is constant
-    interating through nums is n
-space complexity: O(n)
-    we must at most create a set of size n
+time: O(n)
+    just iterating through the entire array of nums
+space: O(1)
+    no extra space needed
 """
